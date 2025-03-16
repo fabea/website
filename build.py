d4a25f4e8c9ffc0deda3c2d885cc1f2e8337c9cc
Alex Yan <alexyan1987@outlook.com>
2025-03-16 11:44:35 +0800

    update postprint

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 from pybtex.database.input import bibtex
 
+
 def get_personal_data():
     name = ['Da "Alex"', "Yan, Ph. D."]
     email = "alexyan1987@outlook.com"
@@ -26,36 +27,44 @@ def get_personal_data():
     bio = """
                     <div id="demo" class="collapse">
                     <span style="font-weight: bold;">Bio:</span>
-                    I hold a Ph.D. degree in Translation (2022–2025) from <a href="https://www.usm.my/" target="_blank">University of Science, Malaysia (USM)</a>. 
-                    With over a decade of teaching experience at 
-                    <a href="https://www.xyafu.edu.cn/" target="_blank">Xinyang Agriculture and Forestry University (XYAFU)</a>, 
-                    I played a key role in establishing the university's Bachelor of Translation and Interpreting (BTI) program. 
-                    I have taught core courses such as Introduction to Translation, Basic and Advanced Interpreting, and Computer-Assisted Translation. 
-                    I have served as the principal investigator or main contributor for 12 social science research projects, focusing on translation and educational practices. 
-                    I have published 16 peer-reviewed papers and received several teaching awards, including the second prize in the Central China Translation Technology Teaching Competition. 
-                    In addition to my academic work, I have provided interpreting services for several international events, including the International Tea Culture Festival and foreign cooperation projects with local governments and universities. 
+                    I hold a Ph.D. degree in Translation (2022–2025) from <a href="https://www.usm.my/" target="_blank">University of Science, Malaysia (USM)</a>.
+                    With over a decade of teaching experience at
+                    <a href="https://www.xyafu.edu.cn/" target="_blank">Xinyang Agriculture and Forestry University (XYAFU)</a>,
+                    I played a key role in establishing the university's Bachelor of Translation and Interpreting (BTI) program.
+                    I have taught core courses such as Introduction to Translation, Basic and Advanced Interpreting, and Computer-Assisted Translation.
+                    I have served as the principal investigator or main contributor for 12 social science research projects, focusing on translation and educational practices.
+                    I have published 16 peer-reviewed papers and received several teaching awards, including the second prize in the Central China Translation Technology Teaching Competition.
+                    In addition to my academic work, I have provided interpreting services for several international events, including the International Tea Culture Festival and foreign cooperation projects with local governments and universities.
                     I also review for mutiple international journals.
                 </div>"""
     footer = """
                 <p class="navbar-text" style="text-align: center;">
-                This website followed the design of <a href="https://m-niemeyer.github.io/" target="_blank">Michael Niemeyer</a> and <a href="https://jonbarron.info/" target="_blank">Jon Barron</a>. 
+                This website followed the design of <a href="https://m-niemeyer.github.io/" target="_blank">Michael Niemeyer</a> and <a href="https://jonbarron.info/" target="_blank">Jon Barron</a>.
                 </p>
     """
     return name, bio_text, social_media, bio, footer
 
+
 def get_author_dict():
     return {
-        'Shuxian Zhang': 'https://www.xyafu.edu.cn/wgyxy/info/1127/7073.htm',
-        'Mansour Amini': 'https://ppblt.usm.my/index.php/lecturer-profile/393-mansour-amini-dr',
-        'Hualing Gong': 'https://s.wanfangdata.com.cn/paper?q=%E4%BD%9C%E8%80%85%3A%22%E9%BE%9A%E5%8D%8E%E7%8E%B2%22%20%E4%BD%9C%E8%80%85%E5%8D%95%E4%BD%8D%3A%20%22%E4%BF%A1%E9%98%B3%E5%86%9C%E6%9E%97%E5%AD%A6%E9%99%A2%22',
-        'Qiongqiong Fan': 'https://www.xyafu.edu.cn/wgyxy/info/1127/7062.htm',
-        'Junyue Wang': 'https://www.xyafu.edu.cn/wgyxy/info/1127/7079.htm',
-        'Shaidatul Kasuma': 'https://ppblt.usm.my/index.php/lecturer-profile/188-shaidatul-akma-adi-kasuma',
-        'Chenjin Jia': 'https://scholar.google.com/citations?hl=en&user=Nk-Ar0IAAAAJ'
-        }
+        "Shuxian Zhang": "https://www.xyafu.edu.cn/wgyxy/info/1127/7073.htm",
+        "Mansour Amini": "https://ppblt.usm.my/index.php/lecturer-profile/393-mansour-amini-dr",
+        "Hualing Gong": "https://s.wanfangdata.com.cn/paper?q=%E4%BD%9C%E8%80%85%3A%22%E9%BE%9A%E5%8D%8E%E7%8E%B2%22%20%E4%BD%9C%E8%80%85%E5%8D%95%E4%BD%8D%3A%20%22%E4%BF%A1%E9%98%B3%E5%86%9C%E6%9E%97%E5%AD%A6%E9%99%A2%22",
+        "Qiongqiong Fan": "https://www.xyafu.edu.cn/wgyxy/info/1127/7062.htm",
+        "Junyue Wang": "https://www.xyafu.edu.cn/wgyxy/info/1127/7079.htm",
+        "Shaidatul Kasuma": "https://ppblt.usm.my/index.php/lecturer-profile/188-shaidatul-akma-adi-kasuma",
+        "Chenjin Jia": "https://scholar.google.com/citations?hl=en&user=Nk-Ar0IAAAAJ",
+    }
 
-def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Kashyap Chitta', 
-                         add_links=True, equal_contribution=None):
+
+def generate_person_html(
+    persons,
+    connection=", ",
+    make_bold=True,
+    make_bold_name="Kashyap Chitta",
+    add_links=True,
+    equal_contribution=None,
+):
     links = get_author_dict() if add_links else {}
     s = ""
 
@@ -64,12 +73,14 @@ def generate_person_html(persons, connection=", ", make_bold=True, make_bold_nam
         equal_contributors = equal_contribution
     for idx, p in enumerate(persons):
         string_part_i = ""
-        for name_part_i in p.get_part('first') + p.get_part('last'): 
+        for name_part_i in p.get_part("first") + p.get_part("last"):
             if string_part_i != "":
                 string_part_i += " "
             string_part_i += name_part_i
         if string_part_i in links.keys():
-            string_part_i = f'<a href="{links[string_part_i]}" target="_blank">{string_part_i}</a>'
+            string_part_i = (
+                f'<a href="{links[string_part_i]}" target="_blank">{string_part_i}</a>'
+            )
         if make_bold and string_part_i == make_bold_name:
             string_part_i = f'<span style="font-weight: bold";>{make_bold_name}</span>'
         if idx < equal_contributors:
@@ -79,84 +90,103 @@ def generate_person_html(persons, connection=", ", make_bold=True, make_bold_nam
         s += string_part_i
     return s
 
+
 def get_paper_entry(entry_key, entry):
-    if 'highlight' in entry.fields.keys():
+    if "highlight" in entry.fields.keys():
         s = """<div style="background-color: #ffffd0; margin-bottom: 3em; padding: 15px"> <div class="row"><div class="col-sm-3 col-4">"""
     else:
         s = """<div style="margin-bottom: 3em;"> <div class="row"><div class="col-md-3 col-4">"""
 
-    s += f"""<img src="{entry.fields['img']}" class="img-fluid img-thumbnail" alt="Project image">"""
+    s += f"""<img src="{entry.fields["img"]}" class="img-fluid img-thumbnail" alt="Project image">"""
     s += """</div><div class="col-md-9 col-8">"""
 
-    if 'award' in entry.fields.keys():
-        s += f"""<a href="{entry.fields['html']}" target="_blank" style="font-weight: bold;">{entry.fields['title']}</a> <span style="color: red;">({entry.fields['award']})</span><br>"""
+    if "award" in entry.fields.keys():
+        s += f"""<a href="{entry.fields["html"]}" target="_blank" style="font-weight: bold;">{entry.fields["title"]}</a> <span style="color: red;">({entry.fields["award"]})</span><br>"""
     else:
-        s += f"""<a href="{entry.fields['html']}" target="_blank" style="font-weight: bold;">{entry.fields['title']}</a> <br>"""
+        s += f"""<a href="{entry.fields["html"]}" target="_blank" style="font-weight: bold;">{entry.fields["title"]}</a> <br>"""
 
-    if 'equal_contribution' in entry.fields.keys():
-        s += f"""{generate_person_html(entry.persons['author'], equal_contribution=int(entry.fields['equal_contribution']))} <br>"""
-    else: 
-        s += f"""{generate_person_html(entry.persons['author'])} <br>"""
-    
-    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+    if "equal_contribution" in entry.fields.keys():
+        s += f"""{generate_person_html(entry.persons["author"], equal_contribution=int(entry.fields["equal_contribution"]))} <br>"""
+    else:
+        s += f"""{generate_person_html(entry.persons["author"])} <br>"""
 
-    artefacts = {'html': 'Web view', 'pdf': 'Self-archieved Paper', 'supp': 'Supplementary', 'video': 'Video', 'poster': 'Poster', 'code': 'Code'}
+    s += f"""<span style="font-style: italic;">{entry.fields["booktitle"]}</span>, {entry.fields["year"]} <br>"""
+
+    artefacts = {
+        "html": "Web view",
+        "pdf": "Postprint archive",
+        "supp": "Supplementary",
+        "video": "Video",
+        "poster": "Poster",
+        "code": "Code",
+    }
     i = 0
-    for (k, v) in artefacts.items():
+    for k, v in artefacts.items():
         if k in entry.fields.keys():
             if i > 0:
-                s += ' / '
+                s += " / "
             s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
             i += 1
         else:
-            print(f'[{entry_key}] Warning: Field {k} missing!')
-    
+            print(f"[{entry_key}] Warning: Field {k} missing!")
+
     cite = "<pre><code>@" + entry.type + "{" + f"{entry_key}, \n"
-    cite += "\tauthor = {" + f"{generate_person_html(entry.persons['author'], make_bold=False, add_links=False, connection=' and ')}" + "}, \n"
-    for entr in ['title', 'booktitle', 'year']:
+    cite += (
+        "\tauthor = {"
+        + f"{generate_person_html(entry.persons['author'], make_bold=False, add_links=False, connection=' and ')}"
+        + "}, \n"
+    )
+    for entr in ["title", "booktitle", "year"]:
         cite += f"\t{entr} = " + "{" + f"{entry.fields[entr]}" + "}, \n"
     cite += """}</pre></code>"""
-    s += " /" + f"""<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{entry_key}" aria-expanded="false" aria-controls="collapseExample" style="margin-left: 2px; margin-top: -2px;">Bibtex</button><div class="collapse" id="collapse{entry_key}"><div class="card card-body">{cite}</div></div>"""
+    s += (
+        " /"
+        + f"""<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{entry_key}" aria-expanded="false" aria-controls="collapseExample" style="margin-left: 2px; margin-top: -2px;">Bibtex</button><div class="collapse" id="collapse{entry_key}"><div class="card card-body">{cite}</div></div>"""
+    )
     s += """ </div> </div> </div>"""
     return s
+
 
 def get_talk_entry(entry_key, entry):
     s = """<div style="margin-bottom: 3em;"> <div class="row"><div class="col-sm-3 col-4">"""
-    s += f"""<img src="{entry.fields['img']}" class="img-fluid img-thumbnail" alt="Project image">"""
-    s += """</div><div class="col-sm-9 col-8">"""   
-    s += f"""<span style="font-weight: bold;">{entry.fields['title']}</span><br>"""
-    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+    s += f"""<img src="{entry.fields["img"]}" class="img-fluid img-thumbnail" alt="Project image">"""
+    s += """</div><div class="col-sm-9 col-8">"""
+    s += f"""<span style="font-weight: bold;">{entry.fields["title"]}</span><br>"""
+    s += f"""<span style="font-style: italic;">{entry.fields["booktitle"]}</span>, {entry.fields["year"]} <br>"""
 
-    artefacts = {'slides': 'Slides', 'video': 'Recording'}
+    artefacts = {"slides": "Slides", "video": "Recording"}
     i = 0
-    for (k, v) in artefacts.items():
+    for k, v in artefacts.items():
         if k in entry.fields.keys():
             if i > 0:
-                s += ' / '
+                s += " / "
             s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
             i += 1
         else:
-            print(f'[{entry_key}] Warning: Field {k} missing!')
+            print(f"[{entry_key}] Warning: Field {k} missing!")
     s += """ </div> </div> </div>"""
     return s
 
+
 def get_publications_html():
     parser = bibtex.Parser()
-    bib_data = parser.parse_file('publication_list.bib')
+    bib_data = parser.parse_file("publication_list.bib")
     keys = bib_data.entries.keys()
     s = ""
     for k in keys:
-        s+= get_paper_entry(k, bib_data.entries[k])
+        s += get_paper_entry(k, bib_data.entries[k])
     return s
+
 
 def get_talks_html():
     parser = bibtex.Parser()
-    bib_data = parser.parse_file('talk_list.bib')
+    bib_data = parser.parse_file("talk_list.bib")
     keys = bib_data.entries.keys()
     s = ""
     for k in keys:
-        s+= get_talk_entry(k, bib_data.entries[k])
+        s += get_talk_entry(k, bib_data.entries[k])
     return s
+
 
 def get_index_html():
     pub = get_publications_html()
@@ -175,14 +205,14 @@ def get_index_html():
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <title>{name[0] + ' ' + name[1] + ' | Assessment and AI Researcher'}</title>
+  <title>{name[0] + " " + name[1] + " | Assessment and AI Researcher"}</title>
   <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="assets/stylesheet.css">
-  
+
 
 </head>
 
@@ -203,7 +233,7 @@ def get_index_html():
                 {social_media}
                 {bio}
             </div>
-            
+
         </div>
         <div class="row" style="margin-top: 3em;">
             <div class="col-sm-12" style="">
@@ -245,11 +275,12 @@ def get_index_html():
     return s
 
 
-def write_index_html(filename='index.html'):
+def write_index_html(filename="index.html"):
     s = get_index_html()
-    with open(filename, 'w',encoding='utf-8') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(s)
-    print(f'Written index content to {filename}.')
+    print(f"Written index content to {filename}.")
 
-if __name__ == '__main__':
-    write_index_html('index.html')
+
+if __name__ == "__main__":
+    write_index_html("index.html")
